3ef522041c966b014ab9d7d87e9f6eecacec1d20
SHI CHEN <s223699413@deakin.edu.au>
2025-12-07 15:27:05 +0800

    test: my first commit on shi-test-branch

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const WelcomeMessage = ({ name }) => {
   return (
     <View>
-      <Text className="text-gray-400">Good Morning, </Text>
+      <Text className="text-gray-400">Good Morning, SHI</Text>
       <Text className="text-white font-semibold text-lg">{name}</Text>
     </View>
   );
